9fa7e966d93dab4d1d8a2a5de82fe97e062207f9
Mohammed Hayath <mdhayath64@gmail.com>
2025-07-16 22:56:28 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello from Simple React App!</h1>
+    </div>
+  );
+}
+
+export default App;
